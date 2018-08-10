eae644fdc8a0f8d904eb3410f24347a64ae6215e
Kailasa Eswaran <40649591+KailasaEswaranIyappan@users.noreply.github.com>
2018-08-10 16:31:51 +0530

    Add files via upload

---
+++ exp32.java
@@ -0,0 +1,14 @@
+class Bike5{  
+	  Bike5(){System.out.println("Vehicle is created");}  
+	}  
+	  
+	class Exp32 extends Bike5{  
+	  Exp32(){  
+	   super();//will invoke parent class constructor  
+	   System.out.println("Bike is created");  
+	  }  
+	  public static void main(String args[]){  
+	   Exp32 b=new Exp32();  
+	        
+	}  
+	}  
