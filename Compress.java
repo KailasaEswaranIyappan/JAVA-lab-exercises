30a20a578b1742ffe3110d6c50a547e237052138
Kailasa Eswaran <40649591+KailasaEswaranIyappan@users.noreply.github.com>
2018-08-22 14:52:44 +0530

    Add files via upload

---
+++ Compress.java
@@ -0,0 +1,21 @@
+package compress;
+import java.io.*;  
+import java.util.zip.*;  
+class Compress{  
+public static void main(String args[]){  
+try{  
+FileInputStream fin=new FileInputStream("Deflater.java");  
+FileOutputStream fout=new FileOutputStream("def.txt");  
+DeflaterOutputStream out=new DeflaterOutputStream(fout);  
+  
+int i;  
+while((i=fin.read())!=-1){  
+out.write((byte)i);  
+out.flush();  
+}  
+fin.close();  
+out.close();  
+}catch(Exception e){System.out.println(e);}  
+System.out.println("rest of the code");  
+}  
+}  
---
+++ DClient.java
@@ -0,0 +1,5 @@
+package compress;
+
+public class DClient {
+
+}
