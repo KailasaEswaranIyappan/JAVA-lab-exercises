031fc223bac9c52bfa91dd9760f85436d99faa2a
Kailasa Eswaran <40649591+KailasaEswaranIyappan@users.noreply.github.com>
2018-08-22 14:53:32 +0530

    Add files via upload

---
+++ Runtime1.java
@@ -0,0 +1,9 @@
+package exep;
+
+public class Runtime1 {  
+	 public static void main(String args[])throws Exception{  
+		  Runtime.getRuntime().exec("notepad");//will open a new notepad  
+		 }  
+		}  
+
+
